e795681525bc35a90cc6039c73f5677a1b794831
RebbitK <andxl9979@gmail.com>
2024-04-23 20:23:18 +0900

    :sparkles: addCoupon 경로수정

--- src/components/coupon/AddComponent.js
+++ src/components/coupon/AddComponent.js
@@ -1,7 +1,7 @@
 import {useState} from "react";
 import {useNavigate} from "react-router-dom";
 import {createCoupon} from "../../api/couponApi";
-import '/src/components/coupon/AddCoupon.css'
+import './AddCoupon.css'
 
 const AddComponent = () => {
   const [requestParam, setRequestParam] = useState(
