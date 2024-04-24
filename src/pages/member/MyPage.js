9440e7f58cb06a80dd25fddf0b146f7bb8b64192
RebbitK <andxl9979@gmail.com>
2024-04-24 16:14:42 +0900

    :recycle: mypage 화면조정

--- src/pages/member/MyPage.js
+++ src/pages/member/MyPage.js
@@ -1,35 +1,19 @@
-import MyCouponListComponent from "../../components/member/MyCouponListComponent";
-import MyPageComponent from "../../components/member/MyPageComponent";
-import PaymentListPageComponent from "../../components/payment/PaymentListComponent";
-import TotalOrderListComponent from "../../components/payment/TotalOrderListComponent";
-import BasicMenu from "../../menus/BasicMenu";
-
 const MyPage = () => {
   return (
       <div className='fixed top-0 left-0 z-[1055] flex flex-col h-screen w-screen overflow-y-auto'>
         <BasicMenu />
-        <div className="w-full flex justify-center">
-          <div className="w-full max-w-screen-lg mx-auto p-4">
-            <MyPageComponent />
-          </div>
+        <div className="w-full max-w-screen-lg mx-auto p-4">
+          <MyPageComponent />
         </div>
-        <div className="w-full flex justify-center">
-          <div className="w-full max-w-screen-lg mx-auto p-4">
-            <MyCouponListComponent />
-          </div>
+        <div className="w-full max-w-screen-lg mx-auto p-4">
+          <MyCouponListComponent />
         </div>
-        <div className="w-full flex justify-center">
-          <div className="w-full max-w-screen-lg mx-auto p-4">
-            <TotalOrderListComponent />
-          </div>
+        <div className="w-full max-w-screen-lg mx-auto p-4">
+          <TotalOrderListComponent />
         </div>
-        <div className="w-full flex justify-center">
-          <div className="w-full max-w-screen-lg mx-auto p-4">
-            <PaymentListPageComponent />
-          </div>
+        <div className="w-full max-w-screen-lg mx-auto p-4">
+          <PaymentListPageComponent />
         </div>
       </div>
   );
 }
-
-export default MyPage;
