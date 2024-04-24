347c619f275d2191a08430d580fa0235a61798e4
RebbitK <andxl9979@gmail.com>
2024-04-24 17:05:56 +0900

    :recycle: product list 변경

--- src/pages/products/ListPage.js
+++ src/pages/products/ListPage.js
@@ -3,8 +3,7 @@ import ListComponent from "../../components/products/ListComponent";
 
 const ListPage = () => {
   return (
-      <div
-          className="p-4 w-full bg-white flex justify-center items-center h-screen overflow-hidden">
+      <div className="p-4 w-full bg-white flex justify-center items-center">
         <ListComponent/>
       </div>
   );
