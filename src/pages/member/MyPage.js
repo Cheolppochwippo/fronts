a147858fb4548093b4ee5d29fd1a10ba699747ab
RebbitK <andxl9979@gmail.com>
2024-04-24 16:22:01 +0900

    :recycle: mypage import수정

--- src/pages/member/MyPage.js
+++ src/pages/member/MyPage.js
@@ -1,3 +1,8 @@
+import MyCouponListComponent from "../../components/member/MyCouponListComponent";
+import MyPageComponent from "../../components/member/MyPageComponent";
+import PaymentListPageComponent from "../../components/payment/PaymentListComponent";
+import TotalOrderListComponent from "../../components/payment/TotalOrderListComponent";
+import BasicMenu from "../../menus/BasicMenu";
 const MyPage = () => {
   return (
       <div className='fixed top-0 left-0 z-[1055] flex flex-col h-screen w-screen overflow-y-auto'>
@@ -17,3 +22,5 @@ const MyPage = () => {
       </div>
   );
 }
+
+export default MyPage;
