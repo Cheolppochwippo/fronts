e1f05741357743dca572387b97bc070bd2fabe46
noeyodeel <sko8044@gmail.com>
2024-04-27 02:31:19 +0900

    feat : roleupdate 구현

--- src/components/member/UserInfoComponent.js
+++ src/components/member/UserInfoComponent.js
@@ -1,52 +1,70 @@
-import {useEffect, useState} from "react";
-import {useNavigate} from "react-router-dom";
-import {viewUserInfo} from "../../api/memberApi";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { updateUserRole, viewUserInfo } from "../../api/memberApi";
 
-const MyPageComponent = ({  }) => {
+
+const MyPageComponent = () => {
   const [info, setInfo] = useState([]);
   const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
 
   useEffect(() => {
     viewUserInfo()
-    .then((user) => {
-      setInfo(user.data);
-      setLoading(false);
-    })
-    .catch((error) => {
-      setLoading(false);
-      console.error("Error fetching Info:", error);
-    });
+      .then((user) => {
+        setInfo(user.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setLoading(false);
+        console.error("Error fetching Info:", error);
+      });
   }, []);
 
   const handleEditProfile = () => {
-    navigate('/member/update');
+    navigate("/member/update");
+  };
+
+  const handleUpdateRole = async () => {
+    try {
+      const response = await updateUserRole();
+      // 성공적으로 역할이 변경되었을 때 추가 로직 구현
+      alert("성공");
+      console.log("Role update success:", response);
+    } catch (error) {
+      alert("실패");
+      console.error("Error updating role:", error);
+      // 역할 변경 실패 시 처리 로직 구현
+    }
   };
 
   return (
-      <div className="m-6 flex flex-col justify-center">
-        <div style={{
-          marginBottom: '1em',
-          fontWeight: 'bold'
-        }}>내정보</div>
-        <div style={{
-          marginBottom: '1em',
-          fontWeight: 'bold'
-        }}>아이디: {info.username}</div>
-        <div style={{
-          marginBottom: '1em',
-          fontWeight: 'bold'
-        }}>역할: {info.role}</div>
-        <div style={{
-          marginBottom: '1em',
-          fontWeight: 'bold'
-        }}>전화번호: {info.phoneNumber}</div>
-        <div style={{
-          marginBottom: '1em',
-          fontWeight: 'bold'
-        }}>수신동의: {info.consent ? '동의함' : '동의하지 않음'}</div>
-        <button className="w-full py-2 px-4 bg-purple-600 text-white rounded hover:bg-purple-700" onClick={handleEditProfile}>내 정보 수정</button>
+    <div className="m-6 flex flex-col justify-center">
+      <div style={{ marginBottom: "1em", fontWeight: "bold" }}>내정보</div>
+      <div style={{ marginBottom: "1em", fontWeight: "bold" }}>
+        아이디: {info.username}
       </div>
+      <div style={{ marginBottom: "1em", fontWeight: "bold" }}>
+        역할: {info.role}
+      </div>
+      <div style={{ marginBottom: "1em", fontWeight: "bold" }}>
+        전화번호: {info.phoneNumber}
+      </div>
+      <div style={{ marginBottom: "1em", fontWeight: "bold" }}>
+        수신동의: {info.consent ? "동의함" : "동의하지 않음"}
+      </div>
+      <button
+        className="w-full py-2 px-4 bg-purple-600 text-white rounded hover:bg-purple-700"
+        onClick={handleEditProfile}
+      >
+        내 정보 수정
+      </button>
+      <button
+        className="w-full py-2 px-4 bg-purple-600 text-white rounded hover:bg-purple-700 mt-2"
+        onClick={handleUpdateRole}
+      >
+        seller로 변경
+      </button>
+    </div>
   );
 };
 
